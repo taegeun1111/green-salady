f28f7892d0f918cb6666197d9900c8cbd56196e7
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-07-28 18:03:13 +0900

    feat : dummy Data list

--- src/components/Foods/FoodList.js
+++ src/components/Foods/FoodList.js
@@ -13,60 +13,74 @@ import salad9 from '../../assets/salad_img/salad9.png';
 const FoodList = () => {
     const DUMMY_SALAD = [
         {
+            id : 's1',
             img: salad1,
             name: '데일리 샐러드',
             price: 6000
         },
         {
+            id : 's2',
             img: salad2,
             name: '그린 샐러드',
             price: 7000
         },
         {
+            id : 's3',
             img: salad3,
             name: '아보카도 샐러드',
             price: 9000
         },
         {
+            id : 's4',
             img: salad4,
             name: '그릴스테이크 샐러드',
             price: 5500
         },
         {
+            id : 's5',
             img: salad5,
             name: '목살 스테이크 샐러드',
             price: 8000
         },
         {
+            id : 's6',
             img: salad6,
             name: '당근라페 소시지 샐러드',
-            price: 600
+            price: 6000
         },
         {
+            id : 's7',
             img: salad7,
             name: '머쉬룸 갈릭치킨 샐러드',
             price: 7000
         },
         {
+            id : 's8',
             img: salad8,
             name: '롤치즈 메추리알 샐러드',
             price: 5500
         },
         {
+            id : 's9',
             img: salad9,
             name: '치킨 샐러드',
             price: 8000
         }
     ]
 
+    const updatePrice =(price)=>{
+        const formatter = new Intl.NumberFormat('ko-KR');
+        return formatter.format(price);
+    }
+
     return (
         <>
             <ul className={classes.food_list_wrapper}>
                 {DUMMY_SALAD.map((salad) => (
-                    <li className={classes.food_list}>
+                    <li className={classes.food_list} key={salad.id}>
                         <img src={salad.img} alt={'food-img'} className={classes.food_img}/>
                         <h1 className={classes.food_title}>{salad.name}</h1>
-                        <h2 className={classes.food_price}>{salad.price}원</h2>
+                        <h2 className={classes.food_price}>{updatePrice(salad.price)}원</h2>
                     </li>
                 ))}
             </ul>
