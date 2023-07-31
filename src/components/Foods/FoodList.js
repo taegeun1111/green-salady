847f2a803ae04e58b97cc76b6f12a34df8119c8f
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-07-31 23:21:28 +0900

    feat : error Code

--- src/components/Foods/FoodList.js
+++ src/components/Foods/FoodList.js
@@ -1,21 +1,39 @@
 import React, {useContext, useEffect, useState} from 'react';
 import classes from './FoodList.module.css';
-import salad1 from '../../assets/salad_img/salad1.png';
-import salad2 from '../../assets/salad_img/salad2.png';
-import salad3 from '../../assets/salad_img/salad3.png';
-import salad4 from '../../assets/salad_img/salad4.png';
-import salad5 from '../../assets/salad_img/salad5.png';
-import salad6 from '../../assets/salad_img/salad6.png';
-import salad7 from '../../assets/salad_img/salad7.png';
-import salad8 from '../../assets/salad_img/salad8.png';
-import salad9 from '../../assets/salad_img/salad9.png';
 import FoodListForm from "./FoodListForm";
 import CartContext from "../store/cart-context";
 import {initializeApp} from "firebase/app";
 import {getStorage, getDownloadURL, ref} from 'firebase/storage';
+import {BarLoader, ClipLoader, RingLoader} from "react-spinners";
 
+/*
+          for (let i = 1; i <= Object.keys(responseData).length; i++) {
+            const imageRef = ref(storage, `salad${i}.png`);
+            getDownloadURL(imageRef).then((url) => {
+              console.log('Image URL:', url);
+              loadedSalad.push({url: url})
+            }).catch((error) => {
+              console.log('Error getting image URL:', error);
+            });
+          }
+          console.log(`After input Img loadedSalad : `, loadedSalad)
+
+          for (const key in responseData) {
+
+            loadedSalad.push({
+              id: key,
+              name: responseData[key].name,
+              price: responseData[key].price,
+            });
+          }
+          console.log(`After input text loadedSalad : `, loadedSalad)
+          setSalad(loadedSalad);
+
+     */
 
 const FoodList = (props) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   const firebaseConfig = {
     apiKey: "AIzaSyBUL-l0ZeL2esLuN4JYUHe9jk5SK6hBrQg",
@@ -27,45 +45,24 @@ const FoodList = (props) => {
     appId: "1:268793420404:web:1cc97efeeb95ae42a9cf21"
   };
 
-
   const [salads, setSalad] = useState([]);
-  useEffect(() => {
 
+  useEffect(() => {
     const fetchSalad = async () => {
       const response = await fetch('https://react-http-abd62-default-rtdb.firebaseio.com/salad.json');
+
+      if (!response.ok){
+        throw new Error('Something went Wrong');
+      }
+
       const responseData = await response.json()
 
       const loadedSalad = [];
-
       //이미지 Url 가져오기
       const app = initializeApp(firebaseConfig);
 
       const storage = getStorage(app); // Firebase app이 이미 초기화된 변수인지 확인 필요
 
-/*
-      for (let i = 1; i <= Object.keys(responseData).length; i++) {
-        const imageRef = ref(storage, `salad${i}.png`);
-        getDownloadURL(imageRef).then((url) => {
-          console.log('Image URL:', url);
-          loadedSalad.push({url: url})
-        }).catch((error) => {
-          console.log('Error getting image URL:', error);
-        });
-      }
-      console.log(`After input Img loadedSalad : `, loadedSalad)
-
-      for (const key in responseData) {
-
-        loadedSalad.push({
-          id: key,
-          name: responseData[key].name,
-          price: responseData[key].price,
-        });
-      }
-      console.log(`After input text loadedSalad : `, loadedSalad)
-      setSalad(loadedSalad);
-
- */
       let imgNum = 1;
       for (const key in responseData) {
         // 이미지 URL 가져오기
@@ -87,8 +84,14 @@ const FoodList = (props) => {
 
       console.log(loadedSalad)
       setSalad(loadedSalad)
+      setIsLoading(false);
     };
-    fetchSalad();
+
+    fetchSalad().catch((error)=>{
+      setIsLoading(false);
+      setHttpError(error.message)
+    });
+
   }, []);
 
 
@@ -107,9 +110,24 @@ const FoodList = (props) => {
       amount: cart.amount,
       price: cart.price
     });
-
-    console.log(`id : ${cart.id}, name : ${cart.name}, amount : ${cart.amount}, img : ${cart.img}`)
+    // console.log(`id : ${cart.id}, name : ${cart.name}, amount : ${cart.amount}, img : ${cart.img}`)
   };
+
+  if (isLoading){
+    return (
+      <section className={classes.loading}>
+        <BarLoader color={'#226632'} />
+      </section>
+    )
+  }
+
+  if (httpError){
+    return (
+      <section className={classes.error}>
+        {httpError}
+      </section>
+    )
+  }
 
   return (
     <>
